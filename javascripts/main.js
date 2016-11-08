cd8cc081890781d8265e1b3f90e5bba2042bec5c
仗剑倚天涯 <wkl_sysfdx@qq.com>
2016-11-08 13:16:28 +0800

    add main.js
    add main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+/*
+* @Author: kelly007
+* @Date:   2016-11-08 13:14:31
+* @Last Modified by:   kelly007
+* @Last Modified time: 2016-11-08 13:14:31
+*/
+
+'use strict';
