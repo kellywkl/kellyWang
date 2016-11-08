737eaa34340c6be21fbb395979780bc46acba362
仗剑倚天涯 <wkl_sysfdx@qq.com>
2016-11-08 13:37:52 +0800

    date
    date

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,13 @@
 * @Author: kelly007
 * @Date:   2016-11-08 13:14:31
 * @Last Modified by:   kelly007
-* @Last Modified time: 2016-11-08 13:14:31
+* @Last Modified time: 2016-11-08 13:37:38
 */
 
 'use strict';
+window.onload=function(){
+
+	var dateTime=new Date();
+	console.log(dateTime);
+
+}
