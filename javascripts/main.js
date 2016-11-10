ade0e487a0a24b801e87a1d14a56e3bcb7bef326
仗剑倚天涯 <wkl_sysfdx@qq.com>
2016-11-10 14:57:54 +0800

    together time
    together time

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@
  * @Author: kelly007
  * @Date:   2016-11-08 13:14:31
  * @Last Modified by:   kelly007
- * @Last Modified time: 2016-11-08 16:43:06
+ * @Last Modified time: 2016-11-10 14:37:30
  */
 
 'use strict';
@@ -17,7 +17,36 @@ Zepto(function($) {
          */
         var currentDate = new Date();
         $(".currentDate").text(formatDateTime(currentDate));
+        timeElapse(currentDate);
     }, 1000);
+
+    /**
+     * [timeElapse description：倒计时]
+     * @return {[type]} [description]
+     */
+    function timeElapse(date){
+        /** @type {Date} [description:在一起时间戳] */
+        var togetherTime=new Date('2015/04/01 19:00:00.0').getTime();
+        /** @type {Date} [description:当前时间戳] */
+        var currentTime =date.getTime();
+        var seconds = (currentTime - togetherTime) / 1000;
+        var days = parseInt(seconds / (3600 * 24));
+        seconds = seconds % (3600 * 24);
+        var hours = parseInt(seconds / 3600);
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        seconds = seconds % 3600;
+        var minutes = parseInt(seconds / 60);
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        seconds = parseInt(seconds % 60);
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        $(".together").html(days+"天 "+hours+"时 "+minutes+"分 "+seconds+"秒");
+    }
 
     /**
      * [formatDateTime description:格式化时间]
